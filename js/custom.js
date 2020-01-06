7a2122d2cb5aa37fa599a091f57bc7c03d63b681
yuriygumeniuk <yuriygumeniuk@gmail.com>
2020-01-06 11:29:24 +0200

    add class 'active'

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,26 @@
-$(function() {
-	$('.banner-slider').slick({
- 			arrows: false,
- 			infinite: true,
-	 		dots: false,
-	  		// autoplay: true,
-		  	// autoplaySpeed: 4000,
-		  	speed: 3000,
- 	});
- 	$('.arrow-right').on('click', function() {
-  		$('.banner-slider').slick('slickNext');
+$(document).ready(function() {
+
+	$('.navigation__item a').click(function(){
+	 	$(this).parents('.navigation').find('.navigation__item').removeClass('active');
+		$(this).parent().addClass('active');
+		return false
 	});
 
- 	$('.arrow-left').on('click', function() {
-  		$('.banner-slider').slick('slickPrev');
-	});
-
- });
-
+	$(function() {
+		$('.banner-slider').slick({
+	 			arrows: false,
+	 			infinite: true,
+		 		dots: false,
+		  		// autoplay: true,
+			  	// autoplaySpeed: 4000,
+			  	speed: 3000,
+	 	});
+	 	$('.arrow-right').on('click', function() {
+	  		$('.banner-slider').slick('slickNext');
+		});
+	 	$('.arrow-left').on('click', function() {
+	  		$('.banner-slider').slick('slickPrev');
+		});
+	 });
+// 
+});
